a7f48ae54b79713de6823faeebd4881a9ba97ce1
Jomana Mohammed <jomanamohammed816@gmail.com>
2026-01-31 13:43:16 +0200

    fix : edited spaces in login and register forms for desktop view

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -70,8 +70,8 @@ const Register = () => {
       {loading && (
         <Spinner fullScreen size="xl" message="Creating your account..." />
       )}
-    <div className="flex min-h-screen justify-center items-center">
-      <div className="flex max-w-[500px] flex-col justify-center items-center px-14 sm:px-10 py-12 lg:px-8 bg-white rounded-lg shadow-lg">
+    <div className="flex min-h-screen justify-center items-center px-6 sm:px-10">
+      <div className="flex w-full max-w-[350px] sm:max-w-md lg:max-w-[500px] flex-col justify-center items-center px-6 py-12 lg:px-8 bg-white rounded-lg shadow-lg">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-4 text-center text-2xl/9 font-bold tracking-tight text-[#111827]">Create your account</h2>
         </div>
